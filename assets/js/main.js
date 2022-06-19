afe91d98aee990cd7e24d0d1da2af5df60f65b69
Isac Wesley <isacwesley@protonmail.com>
2022-06-19 17:45:43 -0300

    push de arquivos

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+function IMC(peso, altura) {
+   return (peso / altura ** 2).toFixed(1);
+}
+
+function verificador(imc) {
+   if (imc > 0.01 && imc < 18.5) {
+      return 1;
+   } else if(imc >= 18.5 && imc < 25) {
+      return 2;
+   } else if (imc >= 25 && imc < 30) {
+      return 3;
+   } else if (imc >= 30 && imc < 35) {
+      return 4;
+   } else if (imc >= 35 && imc < 40) {
+      return 5;
+   } else return 6;
+}
+
+function selector(item) {
+   const tbody = document.querySelector('tbody');
+   const seletores = tbody.querySelectorAll('tr');
+
+   seletores.forEach((element) => {
+      element.classList.remove('bordaBlue');
+   })
+
+   seletores[item - 1].classList.add('bordaBlue');
+}
+
+(() => {
+   const form = document.querySelector("#form-imc");
+
+   form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const peso = document.querySelector("#peso").value;
+      const altura = document.querySelector("#altura").value;
+      const imc = IMC(peso, altura);
+
+      console.log(imc);
+
+      const item = verificador(imc);
+      selector(item);
+   });
+})();
